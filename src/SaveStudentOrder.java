94485e75e05a1158d2cdf2dcc0ae830ef964fb7f
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-30 17:14:54 +0400

    Данным нужны типы.

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,3 +1,28 @@
-public class SaveStudentOrder {
 
+public class SaveStudentOrder {
+    public static void main(String[] args) {
+        StudentOrder studentOrder = new StudentOrder();
+        studentOrder.husbandFirstName = "Alex";
+        studentOrder.husbandLastName = "Nevredimov";
+        studentOrder.wifeFirstName = "Alla";
+        studentOrder.wifeLastName = "Nevredimova";
+
+        StudentOrder studentOrder2 = new StudentOrder();
+        studentOrder2.husbandFirstName = "Mars";
+        studentOrder2.husbandLastName = "Marsov";
+        studentOrder2.wifeFirstName = "Zuzzha";
+        studentOrder2.wifeLastName = "Zuzzheva";
+
+        long ans = saveStudentOrder(studentOrder);
+        System.out.println(ans);
+
+        long ans2 = saveStudentOrder(studentOrder2);
+        System.out.println(ans2);
+    }
+
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 199;
+        System.out.println("saveStudentOrder: " + studentOrder.husbandLastName);
+        return answer;
+    }
 }
